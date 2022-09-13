89561f7f1eca9c1fb4926496c57c1cf3bc82385e
Ruben Marcus <ruben@rubenmarcus.dev>
2022-09-13 04:47:38 +0100

    design

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,31 +1,42 @@
-import { MbButton } from 'mintbase-ui';
-import { useWallet } from '../services/providers/WalletProvider';
+import { MbButton } from "mintbase-ui"
+import { useWallet } from "../services/providers/WalletProvider"
 
 function Header() {
-  const {
-    isConnected, details, signIn, signOut,
-  } = useWallet();
+  const { isConnected, details, signIn, signOut } = useWallet()
 
   const buttonLabel = isConnected
     ? `Sign Out ${details.accountId}`
-    : ' Connect NEAR Wallet';
+    : " Connect NEAR Wallet"
 
   return (
-    <nav className="flex justify-between w-full bg-white sticky top-0 z-40 lg:border-b border-solid border-gray-150">
-      <div className="flex justify-between items-center">
-        <h1 className="font-light text-2xl uppercase p-2 sm:p-4 border-gray-100">
-          Minter
+    <>
+      <nav className='flex  w-full bg-gray-100  sticky top-0 z-40 lg:border-b border-solid border-gray-150'>
+        <h1 className='font-light head-font color-tweet w-full text-2xl justify-items-center 	 text-center align-centerr	 p-2 sm:p-4 border-gray-100'>
+          nftweet.co
         </h1>
-      </div>
-      {/** login/logout with wallet */}
-      <div className="flex items-center sm:mr-2">
-        <MbButton
-          onClick={isConnected ? signOut : signIn}
-          label={buttonLabel}
-        />
-      </div>
-    </nav>
-  );
+      </nav>
+
+      <section className='main-section w-full bg-tweet '>
+        <div className="w-60">
+        <div className='h-full p-20 '>
+          <div className='text-5xl text-black head-font text-center leading-[4rem]	'>
+            Transform your tweets into <span className='bg-black text-white px-2'>NFTs!</span>
+          </div>
+          <ul className='text-2xl pt-10 text-black'>
+          <h3>in only <span className="bg-black text-white px-2">3 steps:</span></h3>
+            <li> - search the tweet, hashtag, or user you want mint it</li>
+            <li> - login to your wallet</li>
+            <li> - mint it</li>
+          </ul>
+        </div>
+        </div>
+      </section>
+      {/* <MbButton
+      onClick={isConnected ? signOut : signIn}2
+      label={buttonLabel}
+    /> */}
+    </>
+  )
 }
 
-export default Header;
+export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,36 @@
-import { GetServerSidePropsContext } from 'next';
-import Head from 'next/head';
-import Header from '../components/Header';
-import Main from '../components/Main';
-import { fetchTwitterPosts } from '../services/fetchTwitterPosts';
+import { GetServerSidePropsContext } from "next"
+import Head from "next/head"
+import Header from "../components/Header"
+import Main from "../components/Main"
+import { fetchTwitterPosts } from "../services/fetchTwitterPosts"
 
-function Minter({media, posts}:any): JSX.Element {
+function Minter({ media, posts }: any): JSX.Element {
+  console.log("media", media)
+  console.log("posts", posts)
 
-  console.log('media', media)
-  console.log('posts', posts)
+  const tweets = posts.filter((post: any) => {
+    return post.attachments?.media_keys?.length > 0
+  })
 
-  const tweets = posts.filter((post:any) => { 
-    return post.attachments?.media_keys?.length > 0;
-   })
+  const screenshotTweets = posts.filter(
+    (item: any) => !tweets.some((tweet: any) => tweet.id === item.id)
+  )
 
-
-  const screenshotTweets =
-   posts.filter((item:any) => !tweets.some((tweet:any) => tweet.id === item.id))
- 
-
-
-
-console.log(tweets,  'tweets')
-console.log(screenshotTweets,  'screenshot Tweets')
-
+  console.log(tweets, "tweets")
+  console.log(screenshotTweets, "screenshot Tweets")
 
   return (
-    <div className="flex flex-1 flex-col min-h-screen text-gray-500">
-      {/* <Head>
-        <title>Mintbase - Simple Minter Example</title>
+    <div className='flex flex-1 flex-col min-h-screen text-gray-500'>
+      <Head>
+        <title>nftweet.co</title>
+
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Work+Sans:wght@400;500;600&display=swap" rel="stylesheet" />
       </Head>
       <Header />
-      <div className="mx-6 sm:mx-24 mt-4 mb-4">
-        <Main />
-      </div> */}
     </div>
-  );
+  )
 }
 
 export const getServerSideProps = async ({
@@ -42,16 +38,13 @@ export const getServerSideProps = async ({
   req,
   query,
 }: GetServerSidePropsContext) => {
- 
+  const { posts } = await fetchTwitterPosts("mintbase")
 
-  const { posts } = await fetchTwitterPosts('mintbase');
-
-  console.log(posts, 'posts')
+  console.log(posts, "posts")
 
   return {
-    props: {media: posts.includes.media, posts: posts.data},
+    props: { media: posts.includes.media, posts: posts.data },
   }
 }
 
-
-export default Minter;
+export default Minter
